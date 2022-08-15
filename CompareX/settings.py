d9cc9179a21908c77a0fdf21307f0060119de17d
Shofolabo Joshua <joshuaemmanuel072@gmail.com>
2022-08-15 07:45:06 +0100

    Update settings.py

--- CompareX/settings.py
+++ CompareX/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("Django_secret_Key", default='django-insecure-%xym-+)dm^6pl!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("django_debug", default=True, cast=bool)
 
-ALLOWED_HOSTS = ["comparexbackend.heroku.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
